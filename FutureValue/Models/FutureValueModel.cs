95f06e90d0416a227107d353c09e565372018b15
Ivan Rodriguez <ivancalvo91@gmail.com>
2022-02-16 22:50:42 -0600

    Add project files.

---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FutureValue.Models
+{
+    public class FutureValueModel
+    {
+        public decimal MontlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal montlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal futureValue = 0;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MontlyInvestment) * (1 + montlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
